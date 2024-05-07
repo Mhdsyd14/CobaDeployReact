88b99af8d76888566bbac9d295c17719210e02e0
DESKTOP-C2OUG3H\MhdSyd014 <irsyadmuhammad954@gmail.com>
2024-05-07 20:00:32 +0700

    Tugas menambahkan component

---
+++ src/component/ClassComponent.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class ClassComponent extends Component {
+  render() {
+    return (
+      <>
+        <h1>Ya kalo ini dari ClassComponent</h1>
+      </>
+    );
+  }
+}
---
+++ src/component/functionComponent.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const FunctionComponent = () => {
+  return (
+    <div>
+      <h1>Hello World ini komponen dari function </h1>
+    </div>
+  );
+};
